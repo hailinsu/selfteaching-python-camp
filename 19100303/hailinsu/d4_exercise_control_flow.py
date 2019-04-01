f54b4ffd4d53cfc8fb25ef4216d88788046c64ca
hailinsu <48571418+hailinsu@users.noreply.github.com>
2019-04-01 18:54:26 +0800

    Create d4_exercise_control_flow.py

---
+++ 19100303/hailinsu/d4_exercise_control_flow.py
@@ -0,0 +1,21 @@
+
+#使用for...in循环打印九九乘法表
+for i in range(1,10):
+    for j in range(1,10):
+       print(i,'x',j,'=',i*j,'/t',end='')
+       ifbi=j:
+          print('')
+          break
+
+
+##使用while循环打印九九乘法表并用条件判断把偶数行除去
+#x=1
+#y=1
+#while(x<10):
+#    while(x%2!=0):
+#        while(y<=x):
+#            print(x,'x',y,'=',x*y,'/t',end='')
+#            y=y+1
+#        else:
+#    x=x+1
+#    y=y+1
